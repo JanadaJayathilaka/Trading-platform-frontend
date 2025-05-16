6fe3f817b80c04851fd79a5c316ca25e40d9edf3
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-16 20:13:02 +0530

    refactor StockDetails component layout and enhance trading functionality

--- src/page/stockDetails/StockDetails.jsx
+++ src/page/stockDetails/StockDetails.jsx
@@ -1,5 +1,19 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { BookmarkFilledIcon } from "@radix-ui/react-icons";
+import { BookmarkIcon, DotIcon } from "lucide-react";
 import React from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import TradingForm from "./TradingForm";
+import StockChart from "../Home/StockChart";
 
 const StockDetails = () => {
   return (
@@ -13,7 +27,48 @@ const StockDetails = () => {
               }
             />
           </Avatar>
+          <div>
+            <div className="flex items-center gap-2">
+              <p>BTC</p>
+              <DotIcon className="text-gray-400" />
+              <p>Bitcoin</p>
+            </div>
+            <div className="flex items-end gap-2">
+              <p className="text-xl font-bold">$103300</p>
+              <p className="text-red-600">
+                <span>-2131241332.567</span>
+                <span>(-0.254535%)</span>
+              </p>
+            </div>
+          </div>
         </div>
+        <div className="flex items-center gap-5">
+          <Button>
+            {true ? (
+              <BookmarkFilledIcon className="h-6 w-6" />
+            ) : (
+              <BookmarkIcon className="h-6 w-6" />
+            )}
+          </Button>
+
+          <Dialog>
+            <DialogTrigger>
+              <Button size={"lg"} className={"bg-amber-50 text-black"}>
+                Trade
+              </Button>
+            </DialogTrigger>
+
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>How much do you want to spend?</DialogTitle>
+              </DialogHeader>
+              <TradingForm />
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+      <div className="mt-20 ">
+        <StockChart />
       </div>
     </div>
   );
