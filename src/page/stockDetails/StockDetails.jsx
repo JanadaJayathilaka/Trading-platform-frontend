c663218535f291946888dbeae0bac4722ee719dc
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-16 18:26:33 +0530

    enhance StockDetails component with layout and avatar display

--- src/page/stockDetails/StockDetails.jsx
+++ src/page/stockDetails/StockDetails.jsx
@@ -1,7 +1,22 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import React from "react";
 
 const StockDetails = () => {
-  return <div>StockDetails</div>;
+  return (
+    <div className="p-5 mt-5">
+      <div className="flex justify-between ">
+        <div className="flex gap-5 items-center">
+          <Avatar>
+            <AvatarImage
+              src={
+                "https://assets.coingecko.com/coins/images/1/standard/bitcoin.png?1696501400"
+              }
+            />
+          </Avatar>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StockDetails;
