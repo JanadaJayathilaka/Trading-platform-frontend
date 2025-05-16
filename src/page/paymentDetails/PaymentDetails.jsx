0f17ee95146fedbc5c9fd07b49538313cd7bb0ba
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-16 14:59:43 +0530

    refactor PaymentDetails component to improve structure and readability

--- src/page/paymentDetails/PaymentDetails.jsx
+++ src/page/paymentDetails/PaymentDetails.jsx
@@ -23,35 +23,38 @@ const PaymentDetails = () => {
     <div className="px-20 ">
       <h1 className="text-3xl font-bold py-10">Payment Details</h1>
 
-      <Card className="border-border">
-        <CardHeader>
-          <CardTitle>Peoples Bank</CardTitle>
-          <CardDescription> AC/NO: ***************3747</CardDescription>
-          <CardContent>
-            <div className="flex items-center ">
-              <p className="w-32">Account Holder</p>
-              <p className="text-gray-400">: Janada Jayathilaka</p>
-            </div>
-            <div className="flex items-center">
-              <p className="w-32">IFSC</p>
-              <p className="text-gray-400">: Peoples32312</p>
-            </div>
-          </CardContent>
-        </CardHeader>
-      </Card>
-      <Dialog>
-        <DialogTrigger>
-          <Button className="py-6 mt-2 bg-white text-black">
-            Add Payment Details
-          </Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Payment Details</DialogTitle>
-          </DialogHeader>
-          <PaymentDetailsForm />
-        </DialogContent>
-      </Dialog>
+      {true ? (
+        <Card className="border-border">
+          <CardHeader>
+            <CardTitle>Peoples Bank</CardTitle>
+            <CardDescription> AC/NO: ***************3747</CardDescription>
+            <CardContent>
+              <div className="flex items-center ">
+                <p className="w-32">Account Holder</p>
+                <p className="text-gray-400">: Janada Jayathilaka</p>
+              </div>
+              <div className="flex items-center">
+                <p className="w-32">IFSC</p>
+                <p className="text-gray-400">: Peoples32312</p>
+              </div>
+            </CardContent>
+          </CardHeader>
+        </Card>
+      ) : (
+        <Dialog>
+          <DialogTrigger>
+            <Button className="py-6 mt-2 bg-white text-black">
+              Add Payment Details
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Payment Details</DialogTitle>
+            </DialogHeader>
+            <PaymentDetailsForm />
+          </DialogContent>
+        </Dialog>
+      )}
     </div>
   );
 };
