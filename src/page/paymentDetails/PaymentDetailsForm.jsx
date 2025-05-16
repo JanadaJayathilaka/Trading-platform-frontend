c9b04a055c00746a55d9795fbb9ecdc4f2ddc57f
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-16 13:24:19 +0530

    add form components and integrate react-hook-form in PaymentDetailsForm

--- src/page/paymentDetails/PaymentDetailsForm.jsx
+++ src/page/paymentDetails/PaymentDetailsForm.jsx
@@ -1,7 +1,52 @@
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const PaymentDetailsForm = () => {
-  return <div>PaymentDetailsForm</div>;
+  const form = useForm({
+    resolver: "",
+    defaultValues: {
+      accountHolderName: "",
+      IFSC: "",
+      accountNumber: "",
+      bankName: "",
+    },
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="px-10 py-2">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default PaymentDetailsForm;
