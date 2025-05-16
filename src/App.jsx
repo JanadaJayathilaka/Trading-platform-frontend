ae3ba347ed76e2ae1ea227cf80cd4f075942c6e1
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-16 20:17:17 +0530

    add Auth component and integrate it into App component

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,30 @@ import WatchList from "./page/watchList/WatchList";
 import Profile from "./page/profile/Profile";
 import SearchCoin from "./page/searchCoin/SearchCoin";
 import NotFound from "./page/notFound/NotFound";
+import Auth from "./page/Auth/Auth";
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/activity" element={<Activity />} />
-        <Route path="/wallet" element={<Wallet />} />
-        <Route path="/withdrawal" element={<Withdrawal />} />
-        <Route path="/payment-details" element={<PaymentDetails />} />
-        <Route path="/market/:id" element={<StockDetails />} />
-        <Route path="/watchlist" element={<WatchList />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/search" element={<SearchCoin />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Auth />
+      {false && (
+        <div>
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/activity" element={<Activity />} />
+            <Route path="/wallet" element={<Wallet />} />
+            <Route path="/withdrawal" element={<Withdrawal />} />
+            <Route path="/payment-details" element={<PaymentDetails />} />
+            <Route path="/market/:id" element={<StockDetails />} />
+            <Route path="/watchlist" element={<WatchList />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/search" element={<SearchCoin />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      )}
     </>
   );
 }
