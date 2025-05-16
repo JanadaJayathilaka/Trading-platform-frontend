0ea12ab362884696225d85846390d6f4f3fe1507
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-16 18:23:34 +0530

    add AccountVerificationForm component for OTP input and email verification

---
+++ src/page/profile/AccountVerificationForm.jsx
@@ -0,0 +1,72 @@
+import { Dialog, DialogClose } from "@/components/ui/dialog";
+import React, { useState } from "react";
+import {
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
+import { Button } from "@/components/ui/button";
+
+const AccountVerificationForm = () => {
+  const [value, setValue] = useState("");
+  const handleSubmit = () => {
+    console.log(value);
+  };
+  return (
+    <div className="flex justify-center">
+      <div className="space-y-5 mt-10 w-full">
+        <div className="flex justify-between items-center">
+          <p>Email : </p>
+          <p>janada123@gmail.com</p>
+          <Dialog>
+            <DialogTrigger>
+              <Button className={"bg-amber-50 text-black"}>Send Otp</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Enter OTP</DialogTitle>
+              </DialogHeader>
+              <div className="py-5 flex gap-10 justify-center items-center">
+                <InputOTP
+                  maxLength={6}
+                  value={value}
+                  onChange={(value) => setValue(value)}
+                >
+                  <InputOTPGroup>
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                    <InputOTPSlot index={2} />
+                  </InputOTPGroup>
+                  <InputOTPSeparator />
+                  <InputOTPGroup>
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                  </InputOTPGroup>
+                </InputOTP>
+                <DialogClose>
+                  <Button
+                    onClick={handleSubmit}
+                    className={"w-[10rem] bg-amber-50 text-black"}
+                  >
+                    submit
+                  </Button>
+                </DialogClose>
+              </div>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountVerificationForm;
