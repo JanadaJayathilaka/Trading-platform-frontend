3031f4d4e6a3e8958f06e9ed1614e3c9987ac2ab
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-18 12:36:57 +0530

    add SignupForm component for user registration

---
+++ src/page/Auth/SignupForm.jsx
@@ -0,0 +1,91 @@
+import { Button } from "@/components/ui/button";
+import { Dialog } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const SignUpForm = () => {
+  const form = useForm({
+    resolver: "",
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="px-10 py-2">
+      <h1 className="text-center font-bold mb-3">Create New Account</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="fullName"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="Full Name"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="Email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="Password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="w-full py-5 bg-white text-black">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default SignUpForm;
