e6a815abad3a36640ac554cc2f2ba91d00885c0e
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-21 15:42:36 +0530

    fix: correct baseUrl to baseURL in axios configuration

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const API_BASE_URL = "http://localhost:8080";
 
 const api = axios.create({
-  baseUrl: API_BASE_URL,
+  baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
