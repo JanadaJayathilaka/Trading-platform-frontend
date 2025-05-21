053d076c82b7b71259d6405be572c5935bf9f15a
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-21 15:42:48 +0530

    feat: update StockDetails component to fetch and display coin details dynamically

--- src/page/stockDetails/StockDetails.jsx
+++ src/page/stockDetails/StockDetails.jsx
@@ -19,31 +19,44 @@ import { useParams } from "react-router-dom";
 import { fetchCoinDetails } from "@/state/Coin/Action";
 
 const StockDetails = () => {
+  const { coin } = useSelector((store) => store);
   const dispatch = useDispatch();
   const { id } = useParams();
 
   useEffect(() => {
-    console.log("Fetching coin with ID:", id);
-    dispatch(fetchCoinDetails({ id, jwt: localStorage.getItem("jwt") }));
+    dispatch(
+      fetchCoinDetails({ coinId: id, jwt: localStorage.getItem("jwt") })
+    );
   }, [id]);
   return (
     <div className="p-5 mt-5">
       <div className="flex justify-between ">
         <div className="flex gap-5 items-center">
           <Avatar>
-            <AvatarImage src={""} />
+            <AvatarImage src={coin.coinDetails?.image.large} />
           </Avatar>
           <div>
             <div className="flex items-center gap-2">
-              <p>BTC</p>
+              <p>{coin.coinDetails?.symbol.toUpperCase()}</p>
               <DotIcon className="text-gray-400" />
-              <p>Bitcoin</p>
+              <p>{coin.coinDetails?.name}</p>
             </div>
             <div className="flex items-end gap-2">
-              <p className="text-xl font-bold">$103300</p>
+              <p className="text-xl font-bold">
+                ${coin.coinDetails?.market_data.current_price.usd}
+              </p>
               <p className="text-red-600">
-                <span>-2131241332.567</span>
-                <span>(-0.254535%)</span>
+                <span>
+                  {coin.coinDetails?.market_data.market_cap_change_24h}
+                </span>
+                <span>
+                  (
+                  {
+                    coin.coinDetails?.market_data
+                      .market_cap_change_percentage_24h
+                  }
+                  %)
+                </span>
               </p>
             </div>
           </div>
@@ -59,9 +72,12 @@ const StockDetails = () => {
 
           <Dialog>
             <DialogTrigger asChild>
-              <Button size={"lg"} className="bg-amber-50 text-black">
+              <button
+                size={"lg"}
+                className={"bg-amber-50 text-black rounded-full h-10 w-25"}
+              >
                 Trade
-              </Button>
+              </button>
             </DialogTrigger>
 
             <DialogContent>
