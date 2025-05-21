02f73a0106f8a115eb43c8461da3c66dcfa36b43
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-21 15:42:29 +0530

    feat: wrap AssetTable in ScrollArea for improved scrolling experience

--- src/page/Home/AssetTable.jsx
+++ src/page/Home/AssetTable.jsx
@@ -11,6 +11,8 @@ import {
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { getCoinList } from "@/state/Coin/Action";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const AssetTable = ({ coin, category }) => {
   const dispatch = useDispatch();
@@ -18,43 +20,46 @@ const AssetTable = ({ coin, category }) => {
 
   return (
     <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">COIN</TableHead>
-          <TableHead>SYMBOL</TableHead>
-          <TableHead>VOLUME</TableHead>
-          <TableHead>MARKET CAP</TableHead>
-          <TableHead>24h</TableHead>
-          <TableHead className="text-right">PRICE</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {coin.map((item, index) => (
-          <TableRow
-            key={item.id}
-            className="hover:bg-border border-1 border-border"
-          >
-            <TableCell
-              onClick={() => navigate(`/market/${item.id}`)}
-              className="font-medium flex items-center gap-2"
-            >
-              <Avatar className="h-10 w-10">
-                <AvatarImage
-                  src={item.image}
-                  className="h-full w-full object-cover"
-                />
-              </Avatar>
-
-              <span>{item.name}</span>
-            </TableCell>
-            <TableCell>{item.symbol}</TableCell>
-            <TableCell>{item.total_volume}</TableCell>
-            <TableCell>{item.market_cap}</TableCell>
-            <TableCell>{item.price_change_percentage_24h}</TableCell>
-            <TableCell className="text-right">${item.current_price}</TableCell>
+      <ScrollArea className={`${category == "all" ? "h-[74vh]" : "h-[82vh]"}`}>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">COIN</TableHead>
+            <TableHead>SYMBOL</TableHead>
+            <TableHead>VOLUME</TableHead>
+            <TableHead>MARKET CAP</TableHead>
+            <TableHead>24h</TableHead>
+            <TableHead className="text-right">PRICE</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
+        </TableHeader>
+        <TableBody>
+          {coin.map((item, index) => (
+            <TableRow
+              key={item.id}
+              className="hover:bg-border border-1 border-border "
+            >
+              <TableCell
+                onClick={() => navigate(`/market/${item.id}`)}
+                className="font-medium flex items-center gap-2 mr-15"
+              >
+                <Avatar className=" shrink-0">
+                  <AvatarImage
+                    src={item.image}
+                    className="h-10 w-10 object-cover rounded full"
+                  />
+                </Avatar>
+                <span>{item.name}</span>
+              </TableCell>
+              <TableCell>{item.symbol}</TableCell>
+              <TableCell>{item.total_volume}</TableCell>
+              <TableCell>{item.market_cap}</TableCell>
+              <TableCell>{item.price_change_percentage_24h}</TableCell>
+              <TableCell className="text-right">
+                ${item.current_price}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </ScrollArea>
     </Table>
   );
 };
